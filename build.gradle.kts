ba7a1d9a0ca256d7c6419ff5233600daba500f5f
Guruth <1669916+Guruth@users.noreply.github.com>
2021-04-19 21:27:32 +0200

    Updated to Kotlin 1.5.0-RC

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,9 +6,9 @@ plugins {
     id("org.springframework.boot") version "2.4.5"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
 
-    kotlin("jvm") version "1.4.32"
-    kotlin("plugin.spring") version "1.4.32"
-    kotlin("plugin.serialization") version "1.4.32"
+    kotlin("jvm") version "1.5.0-RC"
+    kotlin("plugin.spring") version "1.5.0-RC"
+    kotlin("plugin.serialization") version "1.5.0-RC"
 }
 
 group = "sh.weller"
@@ -61,8 +61,6 @@ tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict", "-Xinline-classes", "-Xopt-in=kotlin.RequiresOptIn")
         jvmTarget = "11"
-
-        useIR = true
     }
 }
 
