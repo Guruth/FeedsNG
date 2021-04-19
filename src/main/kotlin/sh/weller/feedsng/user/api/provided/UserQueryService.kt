ca236aaf832fb614dd8842498347f985e64838be
Guruth <1669916+Guruth@users.noreply.github.com>
2021-04-19 20:11:08 +0200

    Added UserQueryServiceImpl.kt

--- src/main/kotlin/sh/weller/feedsng/user/api/provided/UserQueryService.kt
+++ src/main/kotlin/sh/weller/feedsng/user/api/provided/UserQueryService.kt
@@ -2,7 +2,7 @@ package sh.weller.feedsng.user.api.provided
 
 // TODO: Somehow integrate this with Spring ReactiveUserDetailsService
 interface UserQueryService {
-    fun getUserByFeverAPIKey(feverAPIKeyHash: String): User?
+    suspend fun getUserByFeverAPIKey(feverAPIKeyHash: String): User?
 
-    fun getUserByUsername(username: String): User?
+    suspend fun getUserByUsername(username: String): User?
 }
---
+++ src/main/kotlin/sh/weller/feedsng/user/impl/UserQueryServiceImpl.kt
@@ -0,0 +1,18 @@
+package sh.weller.feedsng.user.impl
+
+import org.springframework.stereotype.Service
+import sh.weller.feedsng.user.api.provided.User
+import sh.weller.feedsng.user.api.provided.UserQueryService
+import sh.weller.feedsng.user.api.required.UserRepository
+
+@Service
+class UserQueryServiceImpl(
+    private val userRepository: UserRepository
+) : UserQueryService {
+
+    override suspend fun getUserByFeverAPIKey(feverAPIKeyHash: String): User? =
+        userRepository.getFeverAPIAuthentication(feverAPIKeyHash)
+
+    override suspend fun getUserByUsername(username: String): User? =
+        userRepository.getByUsername(username)
+}
