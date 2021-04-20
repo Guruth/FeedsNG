f9378455891496ee03aa629a58b568b907093131
Guruth <1669916+Guruth@users.noreply.github.com>
2021-04-20 19:42:26 +0200

    StartupService.kt checks if user already exists

--- src/main/kotlin/sh/weller/feedsng/startup/StartupService.kt
+++ src/main/kotlin/sh/weller/feedsng/startup/StartupService.kt
@@ -11,9 +11,11 @@ import sh.weller.feedsng.common.Failure
 import sh.weller.feedsng.common.Success
 import sh.weller.feedsng.feed.api.provided.FeedControlService
 import sh.weller.feedsng.user.api.provided.UserControlService
+import sh.weller.feedsng.user.api.provided.UserQueryService
 
 @Service
 class StartupService(
+    private val userQueryService: UserQueryService,
     private val userControlService: UserControlService,
     private val feedControlService: FeedControlService,
     @Value("classpath:data/StartupOPML.xml") private val startupOPML: Resource
@@ -23,14 +25,19 @@ class StartupService(
     override fun start() {
         logger.info("Starting Startup Service")
         runBlocking {
-            val userId = userControlService.createUser("FeedsNG", "Some.Random.Password!")
-            val feverAPIKey = userControlService.enableFeverAPI(userId)
-            logger.info("Created User FeedsNG - Fever API Key: $feverAPIKey")
+            val userName = "FeedsNG"
+            if (userQueryService.getUserByUsername(userName) != null) {
+                val userId = userControlService.createUser(userName, "Some.Random.Password!")
+                val feverAPIKey = userControlService.enableFeverAPI(userId)
+                logger.info("Created User $userName - Fever API Key: $feverAPIKey")
 
-            val startupOPMLContent = startupOPML.inputStream.reader().readText()
-            when (val importResult = feedControlService.importFromOPML(userId, startupOPMLContent)) {
-                is Failure -> logger.info("Error during import: ${importResult.reason}")
-                is Success -> logger.info("Successfully imported all feeds")
+                val startupOPMLContent = startupOPML.inputStream.reader().readText()
+                when (val importResult = feedControlService.importFromOPML(userId, startupOPMLContent)) {
+                    is Failure -> logger.info("Error during import: ${importResult.reason}")
+                    is Success -> logger.info("Successfully imported all feeds")
+                }
+            } else {
+                logger.info("User $userName already exists - skipping setup.")
             }
         }
         isStarted = true
