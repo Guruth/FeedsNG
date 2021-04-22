c68d394e5d1098adb8ca8fd0c4029ff5b410a675
Guruth <1669916+Guruth@users.noreply.github.com>
2021-04-22 10:38:04 +0200

    Changed RomeFeedFetcherServiceImpl.kt to use Databuffers instead of copying data to bytearrays

--- src/main/kotlin/sh/weller/feedsng/feed/rome/RomeFeedFetcherServiceImpl.kt
+++ src/main/kotlin/sh/weller/feedsng/feed/rome/RomeFeedFetcherServiceImpl.kt
@@ -3,15 +3,18 @@ package sh.weller.feedsng.feed.rome
 import com.rometools.rome.feed.synd.SyndEntry
 import com.rometools.rome.feed.synd.SyndFeed
 import com.rometools.rome.io.SyndFeedInput
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.withContext
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.core.io.buffer.DataBuffer
+import org.springframework.core.io.buffer.DataBufferUtils
 import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
-import org.springframework.web.reactive.function.client.awaitBody
-import org.springframework.web.reactive.function.client.awaitExchange
+import org.springframework.web.reactive.function.client.bodyToFlow
 import reactor.netty.http.client.HttpClient
 import sh.weller.feedsng.common.Failure
 import sh.weller.feedsng.common.Result
@@ -21,8 +24,9 @@ import sh.weller.feedsng.feed.api.provided.FeedData
 import sh.weller.feedsng.feed.api.provided.FeedItemData
 import sh.weller.feedsng.feed.api.required.FeedDetails
 import sh.weller.feedsng.feed.api.required.FeedFetcherService
-import java.io.ByteArrayInputStream
 import java.io.InputStreamReader
+import java.io.PipedInputStream
+import java.io.PipedOutputStream
 import java.time.Duration
 import java.time.Instant
 import java.time.temporal.ChronoUnit
@@ -38,44 +42,61 @@ class RomeFeedFetcherServiceImpl(
                     .responseTimeout(Duration.of(10, ChronoUnit.SECONDS))
             )
         )
-        .codecs { configurer -> configurer.defaultCodecs().maxInMemorySize(16 * 1024 * 1024) }
         .build()
 ) : FeedFetcherService {
 
+    private val scope = CoroutineScope(Dispatchers.Default)
+
+    private val feedBuilder = SyndFeedInput()
+        .apply {
+            isAllowDoctypes = true
+            xmlHealerOn = true
+            isPreserveWireFeed = false
+        }
+
     override suspend fun fetchFeedDetails(feedUrl: String): Result<FeedDetails, String> {
-        val syndFeed = getFeedBytes(feedUrl)
+        val flow = getFeedBytes(feedUrl)
             .onFailure { return it }
-            .toSyndFeed(feedUrl)
+        val syndFeed = withContext(Dispatchers.Default) { toSyndFeedBuffers(flow, feedUrl) }
             .onFailure { return it }
 
         return RomeFeedDetails(feedUrl, syndFeed).asSuccess()
     }
 
-    suspend fun getFeedBytes(feedUrl: String): Result<ByteArray, String> {
+    suspend fun getFeedBytes(feedUrl: String): Result<Flow<DataBuffer>, String> {
         return try {
             client
                 .get()
                 .uri(feedUrl)
-                .awaitExchange {
-                    if (it.statusCode().isError) {
-                        logger.error("Could not fetch feed at $feedUrl: ${it.statusCode().reasonPhrase}")
-                        return@awaitExchange Failure(it.statusCode().reasonPhrase)
-                    }
-                    return@awaitExchange it.awaitBody<ByteArray>().asSuccess()
-                }
+                .retrieve()
+                .bodyToFlow<DataBuffer>()
+                .asSuccess()
         } catch (e: Exception) {
             logger.error("Could not fetch feed $feedUrl: ${e.message}")
             Failure(e.message ?: "Unknown Error")
         }
     }
 
-    private fun ByteArray.toSyndFeed(feedUrl: String): Result<SyndFeed, String> {
-        return try {
-            val feedInput = SyndFeedInput().apply {
-                isAllowDoctypes = true
-                xmlHealerOn = true
+    private fun toSyndFeedBuffers(buffers: Flow<DataBuffer>, feedUrl: String): Result<SyndFeed, String> {
+        val pipedInputStream = PipedInputStream()
+        val pipedOutputStream = PipedOutputStream(pipedInputStream)
+        buffers
+            .onEach {
+                try {
+                    it.asInputStream().copyTo(pipedOutputStream)
+                } finally {
+                    // Make sure the databuffer is released
+                    DataBufferUtils.release(it)
+                }
             }
-            feedInput.build(InputStreamReader(ByteArrayInputStream(this))).asSuccess()
+            .catch { /** Do nothing **/ }
+            .onCompletion { pipedOutputStream.close() }
+            .launchIn(scope)
+
+        return try {
+            InputStreamReader(pipedInputStream).use {
+                feedBuilder.build(it).asSuccess()
+            }
         } catch (e: Exception) {
             logger.error("Could not parse feed of $feedUrl: ${e.message}")
             Failure(e.message ?: "Unknown Error")
