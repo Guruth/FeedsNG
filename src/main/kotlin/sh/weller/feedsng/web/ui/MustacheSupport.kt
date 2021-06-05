8bcc387b6f4ddb60568ddb70fa7a633f4ca1f92a
Guruth <1669916+Guruth@users.noreply.github.com>
2021-06-05 10:24:49 +0200

    Added helper to prefill a model

--- src/main/kotlin/sh/weller/feedsng/web/ui/MustacheSupport.kt
+++ src/main/kotlin/sh/weller/feedsng/web/ui/MustacheSupport.kt
@@ -1,9 +1,16 @@
 package sh.weller.feedsng.web.ui
 
+import kotlinx.coroutines.reactor.awaitSingleOrNull
 import org.springframework.web.reactive.function.server.ServerRequest
-import org.springframework.web.reactive.function.server.awaitPrincipal
 
+suspend fun ServerRequest.getModelMap(): MutableMap<String, Any> {
+    val modelMap = mutableMapOf<String, Any>()
+    addUsernameIfPresent(modelMap)
+    return modelMap
+}
 
-suspend fun ServerRequest.getUsername(): String? = this.awaitPrincipal()?.name
-
-
+private suspend fun ServerRequest.addUsernameIfPresent(modelMap: MutableMap<String, Any>) {
+    val principal = principal().awaitSingleOrNull()
+        ?: return
+    modelMap["username"] = principal.name
+}
