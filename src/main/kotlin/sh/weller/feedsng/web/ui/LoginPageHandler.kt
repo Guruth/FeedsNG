8dc018a3d3eff2c7428b33377d5f3b5ba6916d18
Guruth <1669916+Guruth@users.noreply.github.com>
2021-06-05 10:24:29 +0200

    Working Login Page and logout

--- src/main/kotlin/sh/weller/feedsng/web/ui/LoginPageHandler.kt
+++ src/main/kotlin/sh/weller/feedsng/web/ui/LoginPageHandler.kt
@@ -1,14 +1,12 @@
 package sh.weller.feedsng.web.ui
 
-import kotlinx.coroutines.reactor.awaitSingle
 import org.springframework.security.config.web.server.AuthorizeExchangeDsl
-import org.springframework.security.web.server.csrf.CsrfToken
 import org.springframework.stereotype.Controller
 import org.springframework.web.reactive.function.server.*
 import sh.weller.feedsng.web.support.WebRequestHandler
 
 @Controller
-class MustacheLoginHandler : WebRequestHandler {
+class LoginPageHandler : WebRequestHandler {
     override fun AuthorizeExchangeDsl.addAuthorization() {
         authorize("/login", permitAll)
     }
@@ -18,16 +16,9 @@ class MustacheLoginHandler : WebRequestHandler {
     }
 
     private suspend fun getLoginPage(request: ServerRequest): ServerResponse {
-        val csrfToken: CsrfToken? = request.attributeOrNull("_csrf") as CsrfToken?
-
-        val model = if (csrfToken != null) {
-            mapOf(csrfToken.parameterName to csrfToken.token)
-        } else {
-            emptyMap()
-        }
+        val modelMap = request.getModelMap()
         return ServerResponse.ok()
-            .render("sites/login", model)
-            .awaitSingle()
+            .renderAndAwait("sites/login", modelMap)
     }
-}
 
+}
