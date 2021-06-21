8c6f0d5f219b2c9751c108948e7e402577b0eaa0
Guruth <1669916+Guruth@users.noreply.github.com>
2021-06-21 20:56:08 +0200

    Changed getCSRFPathPatternMatcher to allow all kinds of patternmatchers

--- src/main/kotlin/sh/weller/feedsng/web/support/WebRequestHandler.kt
+++ src/main/kotlin/sh/weller/feedsng/web/support/WebRequestHandler.kt
@@ -1,12 +1,12 @@
 package sh.weller.feedsng.web.support
 
 import org.springframework.security.config.web.server.AuthorizeExchangeDsl
-import org.springframework.security.web.server.util.matcher.PathPatternParserServerWebExchangeMatcher
+import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatcher
 import org.springframework.web.reactive.function.server.RouterFunction
 import org.springframework.web.reactive.function.server.ServerResponse
 
 interface WebRequestHandler {
     fun AuthorizeExchangeDsl.addAuthorization()
-    fun getCSRFPathPatternMatcher(): PathPatternParserServerWebExchangeMatcher?
+    fun getCSRFPathPatternMatcher(): ServerWebExchangeMatcher?
     fun getRouterFunction(): RouterFunction<ServerResponse>
 }
