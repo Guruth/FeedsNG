eccfdf9abcf4a71c6e8981522377a3f482a87123
Guruth <1669916+Guruth@users.noreply.github.com>
2021-08-27 20:57:30 +0200

    Updated FeedUpdateConfiguration to use Duration instead of Long milis

--- src/main/kotlin/sh/weller/feedsng/feed/impl/FeedUpdateServiceImpl.kt
+++ src/main/kotlin/sh/weller/feedsng/feed/impl/FeedUpdateServiceImpl.kt
@@ -1,9 +1,6 @@
 package sh.weller.feedsng.feed.impl
 
 import kotlinx.coroutines.*
-import kotlinx.coroutines.channels.ReceiveChannel
-import kotlinx.coroutines.channels.TickerMode
-import kotlinx.coroutines.channels.ticker
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.toList
 import org.slf4j.Logger
@@ -11,20 +8,22 @@ import org.slf4j.LoggerFactory
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
 import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.boot.convert.DurationUnit
 import org.springframework.context.SmartLifecycle
 import org.springframework.stereotype.Service
 import sh.weller.feedsng.common.onFailure
 import sh.weller.feedsng.feed.api.provided.Feed
 import sh.weller.feedsng.feed.api.required.FeedFetcherService
 import sh.weller.feedsng.feed.api.required.FeedRepository
-import kotlin.time.Duration
+import java.time.Duration
+import java.time.temporal.ChronoUnit
+import java.util.*
+import kotlin.concurrent.fixedRateTimer
 import kotlin.time.ExperimentalTime
 import kotlin.time.measureTime
 
 @Service
 @EnableConfigurationProperties(value = [FeedUpdateConfiguration::class])
-@ObsoleteCoroutinesApi
-@OptIn(ExperimentalTime::class)
 class FeedUpdateServiceImpl(
     private val feedRepository: FeedRepository,
     private val feedFetcherService: FeedFetcherService,
@@ -33,45 +32,44 @@ class FeedUpdateServiceImpl(
 
     private val coroutineScope = CoroutineScope(Dispatchers.Default)
 
-    // This will change in future: https://github.com/Kotlin/kotlinx.coroutines/issues/540
-    private lateinit var updateTicker: ReceiveChannel<Unit>
+    private lateinit var updateTimer: Timer
     private var isStarted = false
 
     override fun start() {
         logger.info("Starting to update feeds every ${feedUpdateConfiguration.updateInterval}ms in ${feedUpdateConfiguration.initialDelay}ms")
-        updateTicker = ticker(
-            delayMillis = feedUpdateConfiguration.updateInterval,
-            initialDelayMillis = feedUpdateConfiguration.initialDelay,
-            mode = TickerMode.FIXED_PERIOD
-        )
-        startUpdatingFeeds()
+
+        updateTimer = fixedRateTimer(
+            initialDelay = feedUpdateConfiguration.initialDelay.toMillis(),
+            period = feedUpdateConfiguration.updateInterval.toMillis()
+        ) {
+            updateFeeds()
+        }
         isStarted = true
     }
 
     override fun stop() {
-        updateTicker.cancel()
+        updateTimer.cancel()
         coroutineScope.cancel("Stopping feed updates")
         isStarted = false
     }
 
     override fun isRunning(): Boolean = isStarted
 
-    private fun startUpdatingFeeds() {
+    @OptIn(ExperimentalTime::class)
+    private fun updateFeeds() {
         coroutineScope.launch {
-            for (tick in updateTicker) {
-                logger.info("Starting to update all feeds.")
-                val updateDuration = measureTime {
-                    feedRepository
-                        .getAllFeeds()
-                        .toList().map {
-                            async {
-                                updateFeed(it)
-                            }
+            logger.info("Starting to update all feeds.")
+            val updateDuration = measureTime {
+                feedRepository
+                    .getAllFeeds()
+                    .toList().map {
+                        async {
+                            updateFeed(it)
                         }
-                        .awaitAll()
-                }
-                logger.info("Finished all updating all feeds in took: ${updateDuration.inWholeSeconds} seconds")
+                    }
+                    .awaitAll()
             }
+            logger.info("Finished all updating all feeds in took: ${updateDuration.inWholeSeconds} seconds")
         }
     }
 
@@ -101,8 +99,9 @@ class FeedUpdateServiceImpl(
 
 @ConfigurationProperties("feedsng.update")
 @ConstructorBinding
-@OptIn(ExperimentalTime::class)
 data class FeedUpdateConfiguration(
-    val initialDelay: Long = Duration.minutes(5).inWholeMilliseconds,
-    val updateInterval: Long = Duration.minutes(10).inWholeMilliseconds
+    @DurationUnit(ChronoUnit.SECONDS)
+    val initialDelay: Duration = Duration.ofSeconds(30),
+    @DurationUnit(ChronoUnit.MINUTES)
+    val updateInterval: Duration = Duration.ofMinutes(5)
 )
